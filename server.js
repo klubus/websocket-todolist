f8bb52ce1855edec3f5a5427b6a0c398fa06e796
Krystian Kluba <klubakrystian@gmail.com>
2026-01-30 16:48:04 +0100

    server

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
 const express = require('express');
+const socket = require('socket.io');
 
 const app = express();
+const io = socket(server);
+
+const tasks = [];
+
+io.on('connection', (socket) => {
+  socket.emit('updateData', tasks);
+  socket.on('addTask', ({ taskId, taskName }) => {
+    tasks.push({ taskId, taskName });
+    socket.broadcast.emit('addTask', { taskId, taskName });
+  });
+  socket.on('removeTask', (taskId) => {
+    const index = tasks.findIndex((e) => e.id === taskId);
+    if (index !== -1) {
+      tasks.splice(index, 1);
+    }
+    socket.broadcast.emit('removeTask', taskId);
+  });
+});
 
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
